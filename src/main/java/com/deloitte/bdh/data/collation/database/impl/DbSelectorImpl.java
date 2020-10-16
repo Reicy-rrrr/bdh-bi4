e2b9e0b4445214c863264bb7961b136a1e979d0f
刘维 <wilcliu@deloitte.com.cn>
2020-10-16 14:22:08 +0800

    #0 包结构调整

--- src/main/java/com/deloitte/bdh/data/collation/database/impl/DbSelectorImpl.java
+++ src/main/java/com/deloitte/bdh/data/collation/database/impl/DbSelectorImpl.java
@@ -2,19 +2,20 @@ package com.deloitte.bdh.data.collation.database.impl;
 
 import com.deloitte.bdh.common.util.NifiProcessUtil;
 import com.deloitte.bdh.common.util.SpringUtil;
+import com.deloitte.bdh.data.collation.dao.bi.BiEtlDatabaseInfMapper;
 import com.deloitte.bdh.data.collation.model.BiEtlDatabaseInf;
 import com.deloitte.bdh.data.collation.database.DbProcess;
 import com.deloitte.bdh.data.collation.database.DbSelector;
 import com.deloitte.bdh.data.collation.database.dto.DbContext;
 import com.deloitte.bdh.data.collation.enums.SourceTypeEnum;
-import com.deloitte.bdh.data.collation.service.BiEtlDatabaseInfService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class DbSelectorImpl implements DbSelector {
-    @Autowired
-    private BiEtlDatabaseInfService biEtlDatabaseInfService;
+    @Resource
+    private BiEtlDatabaseInfMapper biEtlDatabaseInfMapper;
 
     @Override
     public String work(DbContext context) throws Exception {
@@ -37,7 +38,7 @@ public class DbSelectorImpl implements DbSelector {
     }
 
     private void context(DbContext context) {
-        BiEtlDatabaseInf inf = biEtlDatabaseInfService.getById(context.getDbId());
+        BiEtlDatabaseInf inf = biEtlDatabaseInfMapper.selectById(context.getDbId());
         String url = NifiProcessUtil.getDbUrl(inf.getType(), inf.getAddress(), inf.getPort(), inf.getDbName());
         context.setSourceTypeEnum(SourceTypeEnum.values(inf.getType()));
         context.setDbUrl(url);
