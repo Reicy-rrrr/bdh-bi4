48bbd6deb100dc955b4e84922a8232dbb2abd604
刘维 <wilcliu@deloitte.com.cn>
2020-10-27 17:53:27 +0800

    #0 todo 删除无用的代码1

--- src/main/java/com/deloitte/bdh/data/collation/controller/EtlController.java
+++ src/main/java/com/deloitte/bdh/data/collation/controller/EtlController.java
@@ -6,11 +6,7 @@ import com.deloitte.bdh.common.base.RetResponse;
 import com.deloitte.bdh.common.base.RetResult;
 import com.deloitte.bdh.data.collation.integration.EtlService;
 import com.deloitte.bdh.data.collation.model.BiComponent;
-import com.deloitte.bdh.data.collation.model.BiConnections;
-import com.deloitte.bdh.data.collation.model.BiProcessors;
 import com.deloitte.bdh.data.collation.model.request.*;
-import com.deloitte.bdh.data.collation.model.resp.EtlProcessorsResp;
-import com.deloitte.bdh.data.collation.model.resp.EtlRunModelResp;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -19,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * <p>
@@ -44,54 +38,5 @@ public class EtlController {
         return RetResponse.makeOKRsp(etlService.joinResource(request.getData()));
     }
 
-    @ApiOperation(value = "输出组件", notes = "输出组件")
-    @PostMapping("/outProcessors")
-    public RetResult<BiProcessors> outProcessors(@RequestBody @Validated RetRequest<CreateOutProcessorsDto> request) throws Exception {
-        return RetResponse.makeOKRsp(etlService.outProcessors(request.getData()));
-    }
-
-    @ApiOperation(value = "移除组件", notes = "移除组件")
-    @PostMapping("/removeProcessors")
-    public RetResult<Void> removeProcessors(@RequestBody @Validated RetRequest<String> request) throws Exception {
-        etlService.removeProcessors(request.getData());
-        return RetResponse.makeOKRsp();
-    }
-
-    @ApiOperation(value = "关联组件", notes = "关联组件")
-    @PostMapping("/connect")
-    public RetResult<List<BiConnections>> connect(@RequestBody @Validated RetRequest<CreateConnectionsDto> request) throws Exception {
-        return RetResponse.makeOKRsp(etlService.connectProcessors(request.getData()));
-    }
-
-    @ApiOperation(value = "取消关联组件", notes = "取消关联组件")
-    @PostMapping("/cancelConnect")
-    public RetResult<List<BiConnections>> cancelConnect(@RequestBody @Validated RetRequest<String> request) throws Exception {
-        etlService.cancelConnectProcessors(request.getData());
-        return RetResponse.makeOKRsp();
-    }
-
-    @ApiOperation(value = "查看组件详情", notes = "查看组件详情(包含参数集合)")
-    @PostMapping("/getProcessors")
-    public RetResult<EtlProcessorsResp> getProcessors(@RequestBody @Validated RetRequest<String> request) {
-        return RetResponse.makeOKRsp(etlService.getProcessors(request.getData()));
-    }
-
-    @ApiOperation(value = "基于模板编码查看组件列表", notes = "基于模板编码查看组件列表")
-    @PostMapping("/getProcessorsList")
-    public RetResult<List<EtlProcessorsResp>> getProcessorsList(@RequestBody @Validated RetRequest<String> request) {
-        return RetResponse.makeOKRsp(etlService.getProcessorsList(request.getData()));
-    }
-
-    @ApiOperation(value = "运行/停止 model", notes = "运行/停止 model")
-    @PostMapping("/runModel")
-    public RetResult<EtlRunModelResp> runModel(@RequestBody @Validated RetRequest<RunModelDto> request) throws Exception {
-        return RetResponse.makeOKRsp(etlService.runModel(request.getData()));
-    }
-
-    @ApiOperation(value = "预览 ", notes = "预览 ")
-    @PostMapping("/preview")
-    public RetResult<String> preview(@RequestBody @Validated RetRequest<PreviewDto> request) throws Exception {
-        return RetResponse.makeOKRsp(etlService.preview(request.getData()));
-    }
 
 }
