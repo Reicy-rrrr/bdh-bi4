54e94f0b8bb6ea72f349618e05bd67967291b68a
LIJUN <junlicq@deloitte.com.cn>
2020-11-25 10:48:30 +0800

    折线柱状图例适配

--- src/main/java/com/deloitte/bdh/data/analyse/service/impl/datamodel/CategoryDataImpl.java
+++ src/main/java/com/deloitte/bdh/data/analyse/service/impl/datamodel/CategoryDataImpl.java
@@ -8,6 +8,7 @@ import com.deloitte.bdh.data.analyse.model.datamodel.request.BaseComponentDataRe
 import com.deloitte.bdh.data.analyse.model.datamodel.response.BaseComponentDataResponse;
 import com.deloitte.bdh.data.analyse.service.AnalyseDataService;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -38,20 +39,50 @@ public class CategoryDataImpl extends AbstractDataService implements AnalyseData
 
     private void buildCategory(BaseComponentDataRequest request, BaseComponentDataResponse response) {
         List<Map<String, Object>> rows = response.getRows();
+        List<Map<String, Object>> newRows = Lists.newArrayList();
         DataModel dataModel = request.getDataConfig().getDataModel();
         if (CollectionUtils.isNotEmpty(rows) && CollectionUtils.isNotEmpty(dataModel.getCategory())) {
             for (Map<String, Object> row : rows) {
-                List<String> categoryValue = Lists.newArrayList();
+
+                //x轴名称
+                List<String> xList = Lists.newArrayList();
+                for (DataModelField x : dataModel.getX()) {
+                    String colName = x.getId();
+                    if (StringUtils.isNotBlank(x.getAlias())) {
+                        colName = x.getAlias();
+                    }
+                    xList.add(MapUtils.getString(row, colName));
+                }
+                //图例前缀
+                List<String> categoryPrefix = Lists.newArrayList();
                 for (DataModelField category : dataModel.getCategory()) {
                     String colName = category.getId();
                     if (StringUtils.isNotBlank(category.getAlias())) {
                         colName = category.getAlias();
                     }
-                    categoryValue.add(MapUtils.getString(row, colName));
+                    categoryPrefix.add(MapUtils.getString(row, colName));
                 }
-                row.put("category", StringUtils.join(categoryValue, "-"));
+                String categoryPrefixName = StringUtils.join(categoryPrefix, "-");
+                //重新赋值
+                for (DataModelField y : dataModel.getY()) {
+                    String colName = y.getId();
+                    if (StringUtils.isNotBlank(y.getAlias())) {
+                        colName = y.getAlias();
+                    }
+                    Map<String, Object> newRow = Maps.newHashMap();
+                    newRow.put("x", StringUtils.join(xList, "-"));
+                    //如果只有一个值轴，不用按值区分
+                    if (dataModel.getY().size() > 1) {
+                        newRow.put("category", categoryPrefixName + colName);
+                    } else {
+                        newRow.put("category", categoryPrefixName);
+                    }
+                    newRow.put("value", MapUtils.getString(row, colName));
+                    newRows.add(newRow);
+                }
             }
         }
+        response.setRows(newRows);
     }
 
     @Override
@@ -61,9 +92,6 @@ public class CategoryDataImpl extends AbstractDataService implements AnalyseData
         }
         if (CollectionUtils.isEmpty(dataModel.getY())) {
             throw new BizException("度量不能为空");
-        }
-        if (dataModel.getY().size() > 2) {
-            throw new BizException("最多可放入两个度量");
         }
         if (CollectionUtils.isNotEmpty(dataModel.getCategory())) {
             for (DataModelField field : dataModel.getCategory()) {
