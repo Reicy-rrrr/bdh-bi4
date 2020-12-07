282402d724c626ba3c3186eed2af8bdb5b282406
胡卿卿 <fahu@deloitte.com.cn>
2020-12-07 18:02:23 +0800

    地图

--- src/main/java/com/deloitte/bdh/data/analyse/service/impl/datamodel/MapDataImpl.java
+++ src/main/java/com/deloitte/bdh/data/analyse/service/impl/datamodel/MapDataImpl.java
@@ -1,20 +1,81 @@
 package com.deloitte.bdh.data.analyse.service.impl.datamodel;
 
 import com.deloitte.bdh.data.analyse.model.datamodel.DataModel;
+import com.deloitte.bdh.data.analyse.model.datamodel.DataModelField;
 import com.deloitte.bdh.data.analyse.model.datamodel.request.BaseComponentDataRequest;
 import com.deloitte.bdh.data.analyse.model.datamodel.response.BaseComponentDataResponse;
 import com.deloitte.bdh.data.analyse.service.AnalyseDataService;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 
 @Service("mapDataImpl")
 public class MapDataImpl extends AbstractDataService implements AnalyseDataService {
 
-
     @Override
-    public BaseComponentDataResponse handle(BaseComponentDataRequest request) throws Exception {
+    public BaseComponentDataResponse handle(BaseComponentDataRequest request) {
+        DataModel dataModel = request.getDataConfig().getDataModel();
+        List<DataModelField> originalX = Lists.newArrayList(dataModel.getX());
+        if (CollectionUtils.isNotEmpty(dataModel.getX()) && CollectionUtils.isNotEmpty(dataModel.getY())) {
+            dataModel.getY().forEach(field -> dataModel.getX().add(field));
+        }
+        if (CollectionUtils.isNotEmpty(dataModel.getX()) && CollectionUtils.isNotEmpty(dataModel.getCategory())) {
+            dataModel.getCategory().forEach(field -> dataModel.getX().add(field));
+        }
+        BaseComponentDataResponse response = execute(buildSql(request.getDataConfig().getDataModel()));
+        request.getDataConfig().getDataModel().setX(originalX);
+        response.setRows(buildCategory(request, response.getRows(), dataModel.getY()));
+        return response;
+    }
 
-        return execute(buildSql(request.getDataConfig().getDataModel()));
+    private List<Map<String, Object>> buildCategory(BaseComponentDataRequest request, List<Map<String, Object>> rows, List<DataModelField> yList) {
+
+        List<Map<String, Object>> newRows = Lists.newArrayList();
+        DataModel dataModel = request.getDataConfig().getDataModel();
+        for (Map<String, Object> row : rows) {
+
+            //x轴名称
+            List<String> xList = Lists.newArrayList();
+            for (DataModelField x : dataModel.getX()) {
+                String colName = x.getId();
+                if (StringUtils.isNotBlank(x.getAlias())) {
+                    colName = x.getAlias();
+                }
+                xList.add(MapUtils.getString(row, colName));
+            }
+            //其他参数
+            List<String> categoryPrefix = Lists.newArrayList();
+            for (DataModelField category : dataModel.getCategory()) {
+                String colName = category.getId();
+                if (StringUtils.isNotBlank(category.getAlias())) {
+                    colName = category.getAlias();
+                }
+                categoryPrefix.add(MapUtils.getString(row, colName));
+            }
+            //重新赋值
+            for (DataModelField y : yList) {
+                String colName = y.getId();
+                if (StringUtils.isNotBlank(y.getAlias())) {
+                    colName = y.getAlias();
+                }
+                Map<String, Object> newRow = Maps.newHashMap();
+                newRow.put("name", StringUtils.join(xList, "-"));
+                if (CollectionUtils.isNotEmpty(categoryPrefix)) {
+                    newRow.put("category", categoryPrefix);
+                }
+
+                newRow.put("value", MapUtils.getObject(row, colName));
+                newRows.add(newRow);
+            }
+        }
+        return newRows;
     }
 
     @Override
