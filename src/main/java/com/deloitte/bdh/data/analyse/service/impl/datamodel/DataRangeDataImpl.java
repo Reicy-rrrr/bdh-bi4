6208bfe324acf67342fb29bd93e227b54d4af888
胡卿卿 <fahu@deloitte.com.cn>
2020-12-18 10:33:35 +0800

    自定义SQL，未找到数据集BUG

--- src/main/java/com/deloitte/bdh/data/analyse/service/impl/datamodel/DataRangeDataImpl.java
+++ src/main/java/com/deloitte/bdh/data/analyse/service/impl/datamodel/DataRangeDataImpl.java
@@ -1,13 +1,18 @@
 package com.deloitte.bdh.data.analyse.service.impl.datamodel;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.deloitte.bdh.common.exception.BizException;
 import com.deloitte.bdh.data.analyse.model.datamodel.DataModel;
 import com.deloitte.bdh.data.analyse.model.datamodel.DataModelField;
 import com.deloitte.bdh.data.analyse.model.datamodel.request.ComponentDataRequest;
 import com.deloitte.bdh.data.analyse.model.datamodel.response.BaseComponentDataResponse;
 import com.deloitte.bdh.data.analyse.service.AnalyseDataService;
+import com.deloitte.bdh.data.collation.model.BiDataSet;
+import com.deloitte.bdh.data.collation.service.BiDataSetService;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Author:LIJUN
@@ -17,11 +22,19 @@ import org.springframework.stereotype.Service;
 @Service("dataRangeDataImpl")
 public class DataRangeDataImpl extends AbstractDataService implements AnalyseDataService {
 
+    @Resource
+    private BiDataSetService dataSetService;
+
     @Override
     public BaseComponentDataResponse handle(ComponentDataRequest request) {
         DataModel dataModel = request.getDataConfig().getDataModel();
         DataModelField field = dataModel.getX().get(0);
-        String sql = "SELECT MIN(" + field.getId() + ") AS MIN, MAX(" + field.getId() + ") AS MAX FROM " + dataModel.getTableName();
+        BiDataSet dataSet = dataSetService.getOne(new LambdaQueryWrapper<BiDataSet>()
+                .eq(BiDataSet::getTableDesc, dataModel.getTableName()));
+        if (null == dataSet) {
+            throw new RuntimeException("未在数据集找到目标对象");
+        }
+        String sql = "SELECT MIN(" + field.getId() + ") AS MIN, MAX(" + field.getId() + ") AS MAX FROM " + dataSet.getTableName();
         return execute(dataModel, sql);
     }
 
